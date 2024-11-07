1b2abaddaa2c0499708979819fbfab0ba8ffe5aa
huseyinsusever <73393748+huseyinsusever@users.noreply.github.com>
2024-11-07 16:52:05 +0300

    Add files via upload

---
+++ js-Javasyntax.js
@@ -0,0 +1,16 @@
+//var x;
+//let y;
+//
+//x = 5;
+//y = 6;
+//let z = x + y;
+
+//let x, y;
+//x = 5;
+//y = 6;
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+document.getElementById("demo").innerHTML =
+"The total is: " + total;
