c6e78dc4cab45fe412168f8d51792f6d122ba54a
huseyinsusever <73393748+huseyinsusever@users.noreply.github.com>
2024-11-16 22:04:22 +0300

    Add files via upload

---
+++ js-homework.js
@@ -0,0 +1,10 @@
+//console.log(window);
+
+//alert("Hello what is your name:")
+
+
+let name1 = prompt("hello what is your name: ");
+let age = prompt("what is your age: ");
+console.log("Your name :  " + name1);
+console.log("Age : " + age);
+
